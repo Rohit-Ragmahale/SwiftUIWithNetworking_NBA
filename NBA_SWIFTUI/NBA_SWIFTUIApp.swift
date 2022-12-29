248f3860fff6e0c19d71006ad45b9153ae974cff
Rohit Ragmahale <rohitragmahale.mobile@gmail.com>
2022-12-29 22:22:20 +0000

    Initial Commit

---
+++ NBA_SWIFTUI/NBA_SWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_SWIFTUIApp.swift
+//  NBA_SWIFTUI
+//
+//  Created by Rohit Ragmahale on 29/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct NBA_SWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
